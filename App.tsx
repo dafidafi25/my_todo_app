793f559c6654faf86bead489b0f7dfabaab9807d
dafidafi25 <dafidafi25@gmail.com>
2022-07-23 20:54:25 +0700

    first_file_conf

---
+++ App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'light-content'} />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@assets': '.src/assets/*',
+          '@components': '.src/components/*',
+          '@styles': '.src/styles/*',
+          '@themes': '.src/themes/*',
+        },
+      },
+    ],
+  ],
+};
